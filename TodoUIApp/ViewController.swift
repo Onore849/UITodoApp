6f17f1ee11f9c4bde35afd50da73938a212eb8a0
Onore849 <rion3376@gmail.com>
2020-11-03 18:41:05 +0900

    Initial Commit

---
+++ TodoUIApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoUIApp
+//
+//  Created by 野澤拓己 on 2020/11/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
